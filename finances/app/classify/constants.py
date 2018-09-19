eabf047387d4223a780bed849f677358909ec51a
Julie Qiu <julieyeqiu@gmail.com>
2018-09-19 22:56:44 +0200

    updated constants -> different dict structure

--- finances/app/classify/constants.py
+++ finances/app/classify/constants.py
@@ -1,51 +1,72 @@
-def generate_category_to_phrases() -> dict:
-    def phrases_list(filename: str) -> list:
-        with open('/Users/julie/Code/finances/finances/app/classify/filters/' + filename, 'r') as f:
-            words = f.readlines()
-            return [word[:-1] for word in words]
+# FOOD
+def phrases_list(filename: str) -> list:
+    with open('/Users/julie/Code/finances/finances/app/classify/filters/' + filename, 'r') as f:
+        words = f.readlines()
+        return [word[:-1] for word in words]
 
-    PHRASES_ALCOHOL = phrases_list('alcohol.txt')
-    PHRASES_COFFEE = phrases_list('coffee.txt')
-    PHRASES_ENTERTAINMENT = phrases_list('entertainment.txt')
-    PHRASES_FOOD = phrases_list('food.txt')
-    PHRASES_SKIPPED = phrases_list('skipped.txt')
-    PHRASES_TRAVEL = phrases_list('travel.txt')
+CATEGORY_TO_PHRASES = {
+    'Beauty': {
+        'nails': ['NAILS', 'DREAM BLUE BEAUTY'],
+        'spa': ['SPA'],
+    },
 
-    return {
-        'aws': ['AWS'],
-        'alcohol': PHRASES_ALCOHOL,
-        'amazon': ['Amazon'],
-        'atm': ['ATM WITHDRAW', 'ATM DEBIT'],
-        'audible': ['Audible'],
-        'checks': ['REMOTE ONLINE DEPOSIT', 'ATM CHECK DEPOSIT', 'DEPOSIT *MOBILE NY'],
-        'coffee': PHRASES_COFFEE,
-        'customer_withdrawal': ['Customer Withdrawal Image'],
-        'entertainment': PHRASES_ENTERTAINMENT,
-        'equinox': ['EQUINOX'],
-        'food': PHRASES_FOOD,
-        'grocery': ['TRADER JOE', 'WHOLEFDS'],
-        'income': ['SPRING NYC', 'SHOPSPRING', 'JELLO LABS DES:DIR DEP ID'],
-        'bonus': ['$150 for New Savings', '$200 for New Checking'],
-        'laundry': ['LAUNDRY LAND'],
-        'lyft': ['Lyft', 'Uber'],
-        'mta': ['MTA'],
-        'beauty': ['NAILS', 'DREAM BLUE BEAUTY', 'SPA'],
-        'paypal': ['PAYPAL'],
-        'physical_therapy': ['RECOVERY PHYSICAL THERAPY'],
-        'reimbursed': ['Expensify', 'ELASTICSEARCH JAN3'],
-        'rent': ['STUYVESANT TOWN'],
-        'skipped': PHRASES_SKIPPED,
-        'stitch_fix': ['STITCH FIX'],
-        'taxi': ['NYCTAXI', 'TAXI SVC'],
-        'travel':PHRASES_TRAVEL,
-        'venmo': ['Venmo', 'Zelle'],
-        'weight_watchers': ['WEIGHTWATCHERS'],
-        'walgreens_cvs': ['WALGREENS', 'CVS'],
-        'shopping': ['BOOK', 'BEST BUY'],
-        'interview_prep': ['INTERVIEWCAKE'],
-        'repairs': ['MAX SHOE REPAIR'],
-        'health': ['MOUNT SINAI'],
+    'Entertainment': {
+        'entertainment': phrases_list('entertainment.txt'),
+        'travel': phrases_list('travel.txt'),
         'wedding': ['ZOLA'],
+    },
+
+    'Food': {
+        'alcohol': phrases_list('alcohol.txt'),
+        'coffee': phrases_list('coffee.txt'),
+        'food': phrases_list('food.txt'),
+        'grocery': ['TRADER JOE', 'WHOLEFDS'],
+    },
+
+    'Health': {
+        'health': ['MOUNT SINAI'],
+        'physical_therapy': ['RECOVERY PHYSICAL THERAPY'],
     }
 
-CATEGORY_TO_PHRASES = generate_category_to_phrases()
+    'Income': {
+        'bonus': ['$150 for New Savings', '$200 for New Checking'],
+        'checks': ['REMOTE ONLINE DEPOSIT', 'ATM CHECK DEPOSIT', 'DEPOSIT *MOBILE NY'],
+        'income': ['SPRING NYC', 'SHOPSPRING', 'JELLO LABS DES:DIR DEP ID'],
+        'reimbursed': ['Expensify', 'ELASTICSEARCH JAN3'],
+    },
+
+    'Monthly': {
+        'equinox': ['EQUINOX'],
+        'laundry': ['LAUNDRY LAND'],
+        'rent': ['STUYVESANT TOWN'],
+        'weight_watchers': ['WEIGHTWATCHERS'],
+    },
+
+    'Shopping': {
+        'amazon': ['Amazon'],
+        'audible': ['Audible'],
+        'aws': ['AWS'],
+        'interview_prep': ['INTERVIEWCAKE'],
+        'repairs': ['MAX SHOE REPAIR'],
+        'shopping': ['BOOK', 'BEST BUY'],
+        'stitch_fix': ['STITCH FIX'],
+        'walgreens_cvs': ['WALGREENS', 'CVS'],
+    },
+
+    'Transportation': {
+        'lyft': ['Lyft', 'Uber'],
+        'mta': ['MTA'],
+        'taxi': ['NYCTAXI', 'TAXI SVC'],
+    },
+
+    'Withdrawal': {
+        'atm': ['ATM WITHDRAW', 'ATM DEBIT'],
+        'customer_withdrawal': ['Customer Withdrawal Image'],
+        'paypal': ['PAYPAL'],
+        'venmo': ['Venmo', 'Zelle'],
+    },
+
+    'Skipped': {
+        'skipped': {'skipped': phrases_list('skipped.txt')},
+    }
+}
